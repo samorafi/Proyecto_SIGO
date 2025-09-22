9951cb5210195e4a509ba1e9beaa2a47ab03e15a
jvkevin2001 <Kevin.pereira@pandatechla.com>
2025-09-21 20:07:01 -0600

    FEAT-CREACION DE PROYECTO DEL BACKEND

---
+++ Backend-SIGO Ufide/SIGO.Domain (Proyecto Biblioteca de Clases .NET)/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SIGO.Domain__Proyecto_Biblioteca_de_Clases_.NET_
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Backend-SIGO Ufide/SIGO.Infrastructure (Proyecto Biblioteca de Clases .NET)/DependencyInjection.cs
@@ -0,0 +1,9 @@
+﻿//(Para registrar servicios de esta capa)
+
+namespace SIGO.Infrastructure__Proyecto_Biblioteca_de_Clases_.NET_
+{
+    public class DependencyInjection
+    {
+
+    }
+}
