0fd3f0edcd272093651d6037779d0f728bb8388c
Harlyn Luna <harlynlunab@gmail.com>
2026-02-12 20:56:47 -0600

    Upd - Vista de Oferta Historico

---
+++ Front-End - SIGO Ufide/src/pages/ofertas/OfertasHistoricoV2.jsx
@@ -0,0 +1,5 @@
+import OfertasPagedTable from "./components/OfertasPagedTable";
+
+export default function OfertasHistoricoV2() {
+  return <OfertasPagedTable category={3} title="Historico de Ofertas" />;
+}
