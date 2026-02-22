4b5acc359c6dcf2de1da4042f793405b18b0c263
samorafi <sebasmora172522@gmail.com>
2026-02-21 19:09:25 -0600

    QA on Enviroment AWS
    Cambios necesarios para subir el proyecto a AWS

--- Backend-SIGO Ufide/Backend_SIGO/Program.cs
+++ Backend-SIGO Ufide/Backend_SIGO/Program.cs
@@ -14,12 +14,34 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+<<<<<<< Updated upstream
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
+=======
+// 10. Pipeline HTTP
+// Habilitar Swagger siempre (o condicionalmente si prefieres)
+// Nota: Si usas Docker con 'ENV ASPNETCORE_ENVIRONMENT=Development', entrará aquí.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+var enableSwagger = app.Environment.IsDevelopment() ||
+                    builder.Configuration.GetValue<bool>("EnableSwagger");
+
+if (enableSwagger)
+>>>>>>> Stashed changes
 {
     app.MapOpenApi();  
 }
 
+<<<<<<< Updated upstream
+=======
+app.MapGet("/health", () => Results.Ok("OK"));
+
+app.UseCors("AllowAll");
+>>>>>>> Stashed changes
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
