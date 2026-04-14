97897ef78ae6285ba1df06ecd56a76c1e05e5cd0
Harlyn Luna <harlynlunab@gmail.com>
2026-04-13 20:59:54 -0600

    Fix - Ajuste en duplicar ofertas

--- Front-End - SIGO Ufide/src/pages/ofertas/modals/modalDuplicarOfertas_v2.jsx
+++ Front-End - SIGO Ufide/src/pages/ofertas/modals/modalDuplicarOfertas_v2.jsx
@@ -227,36 +227,7 @@ export default function ModalDuplicarOfertas_v2({
           </Select>
         </div>
 
-        {/* Banner informativo: Warning */}
-        {infoCanceladas && (
-          <div className="sm:col-span-2 mb-2">
-            <div className="flex gap-3 rounded-lg border-l-4 border-amber-500 bg-amber-50 p-4 shadow-sm">
-              {/* Icono de Advertencia (Warning) */}
-              <div className="shrink-0">
-                <svg
-                  className="h-5 w-5 text-amber-600"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
 
-              <div>
-                <Typography variant="small" className="font-bold text-amber-900">
-                  Aviso sobre ofertas canceladas
-                </Typography>
-                <Typography variant="small" className="text-amber-800 leading-relaxed">
-                  {infoCanceladas.replace('Importarte: ', '').replace('Importante: ', '')}
-                </Typography>
-              </div>
-            </div>
-          </div>
-        )}
 
       </div>
     </AppModal>
